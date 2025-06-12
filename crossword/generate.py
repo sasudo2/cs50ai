623f6b7138e2a9095addedc0485382559fa69fdd
sasudo2 <sanjayashrestha777@gmail.com>
2025-06-12 08:05:28 +0000

    knearestClassification

---
+++ crossword/generate.py
@@ -0,0 +1,300 @@
+import sys
+
+from crossword import *
+
+
+class CrosswordCreator():
+
+    def __init__(self, crossword):
+        """
+        Create new CSP crossword generate.
+        """
+        self.crossword = crossword
+        self.domains = {
+            var: self.crossword.words.copy()
+            for var in self.crossword.variables
+        }
+
+    def letter_grid(self, assignment):
+        """
+        Return 2D array representing a given assignment.
+        """
+        letters = [
+            [None for _ in range(self.crossword.width)]
+            for _ in range(self.crossword.height)
+        ]
+        for variable, word in assignment.items():
+            direction = variable.direction
+            for k in range(len(word)):
+                i = variable.i + (k if direction == Variable.DOWN else 0)
+                j = variable.j + (k if direction == Variable.ACROSS else 0)
+                letters[i][j] = word[k]
+        return letters
+
+    def print(self, assignment):
+        """
+        Print crossword assignment to the terminal.
+        """
+        letters = self.letter_grid(assignment)
+        for i in range(self.crossword.height):
+            for j in range(self.crossword.width):
+                if self.crossword.structure[i][j]:
+                    print(letters[i][j] or " ", end="")
+                else:
+                    print("█", end="")
+            print()
+
+    def save(self, assignment, filename):
+        """
+        Save crossword assignment to an image file.
+        """
+        from PIL import Image, ImageDraw, ImageFont
+        cell_size = 100
+        cell_border = 2
+        interior_size = cell_size - 2 * cell_border
+        letters = self.letter_grid(assignment)
+
+        # Create a blank canvas
+        img = Image.new(
+            "RGBA",
+            (self.crossword.width * cell_size,
+             self.crossword.height * cell_size),
+            "black"
+        )
+        font = ImageFont.truetype("assets/fonts/OpenSans-Regular.ttf", 80)
+        draw = ImageDraw.Draw(img)
+
+        for i in range(self.crossword.height):
+            for j in range(self.crossword.width):
+
+                rect = [
+                    (j * cell_size + cell_border,
+                     i * cell_size + cell_border),
+                    ((j + 1) * cell_size - cell_border,
+                     (i + 1) * cell_size - cell_border)
+                ]
+                if self.crossword.structure[i][j]:
+                    draw.rectangle(rect, fill="white")
+                    if letters[i][j]:
+                        _, _, w, h = draw.textbbox((0, 0), letters[i][j], font=font)
+                        draw.text(
+                            (rect[0][0] + ((interior_size - w) / 2),
+                             rect[0][1] + ((interior_size - h) / 2) - 10),
+                            letters[i][j], fill="black", font=font
+                        )
+
+        img.save(filename)
+
+    def solve(self):
+        """
+        Enforce node and arc consistency, and then solve the CSP.
+        """
+        self.enforce_node_consistency()
+        self.ac3()
+        return self.backtrack(dict())
+
+    def enforce_node_consistency(self):
+        """
+        Update `self.domains` such that each variable is node-consistent.
+        (Remove any values that are inconsistent with a variable's unary
+         constraints; in this case, the length of the word.)
+        """
+        
+        for var in self.domains:
+            word_to_remove = set()
+            for word in self.domains[var]:
+                if var.length != len(word):
+                     word_to_remove.add(word)
+
+            for word in word_to_remove:
+                self.domains[var].remove(word)
+        
+    def revise(self, x, y):
+        """
+        Make variable `x` arc consistent with variable `y`.
+        To do so, remove values from `self.domains[x]` for which there is no
+        possible corresponding value for `y` in `self.domains[y]`.
+
+        Return True if a revision was made to the domain of `x`; return
+        False if no revision was made.
+        """
+        overlap = self.crossword.overlaps[x, y]
+        revise = False
+        if overlap:
+            words_to_remove = set()
+            for word_x in self.domains[x]:
+                words = set()
+                for word_y in self.domains[y]:
+                    if word_x[overlap[0]] == word_y[overlap[1]]:
+                        words.add(word_x)
+                
+                if word_x not in words:
+                    words_to_remove.add(word_x)
+
+            for word in words_to_remove:
+                revise = True
+                self.domains[x].remove(word)
+        
+        return revise
+
+    def ac3(self, arcs=None):
+        """
+        Update `self.domains` such that each variable is arc consistent.
+        If `arcs` is None, begin with initial list of all arcs in the problem.
+        Otherwise, use `arcs` as the initial list of arcs to make consistent.
+
+        Return True if arc consistency is enforced and no domains are empty;
+        return False if one or more domains end up empty.
+        """
+        if arcs == None:
+            arcs = []
+            for x in self.crossword.variables:
+                neighbors = self.crossword.neighbors(x)
+                for y in neighbors:
+                    arcs.append((x, y))
+
+        while arcs:
+            x, y = arcs.pop()
+            if self.revise(x, y):
+                if len(self.domains[x]) == 0:
+                    return False
+                neighbours = self.crossword.neighbors(x)
+                neighbours.discard(y)
+                for z in neighbours:
+                    arcs.append((z, x))
+
+        return True
+
+    def assignment_complete(self, assignment):
+        """
+        Return True if `assignment` is complete (i.e., assigns a value to each
+        crossword variable); return False otherwise.
+        """
+        for var in assignment:
+            if assignment[var] == None:
+                return False    
+
+        for var in self.crossword.variables:
+            if var not in assignment:
+                return False  
+        return True
+    
+    def consistent(self, assignment):
+        """
+        Return True if `assignment` is consistent (i.e., words fit in crossword
+        puzzle without conflicting characters); return False otherwise.
+        """
+        for var in assignment:
+            if len(assignment[var]) != var.length:
+                return False
+            neighbors = self.crossword.neighbors(var)
+            for neighbor in neighbors:
+                if neighbor not in assignment:
+                    continue
+                if self.crossword.overlaps[var, neighbor] == None:
+                    return False
+                i, j = self.crossword.overlaps[var, neighbor]
+                if assignment[var][i] != assignment[neighbor][j]:
+                    return False
+
+        x = [z for z in assignment.values()]
+        if len(x) != len(set(x)):
+            return False
+        
+        return True
+
+    def order_domain_values(self, var, assignment):
+        """
+        Return a list of values in the domain of `var`, in order by
+        the number of values they rule out for neighboring variables.
+        The first value in the list, for example, should be the one
+        that rules out the fewest values among the neighbors of `var`.
+        """
+        neighbors = self.crossword.neighbors(var)
+        domain_var = self.domains[var]
+        dictionary_for_ordering = {x: 0 for x in domain_var}
+        for neighbor in neighbors:
+            if neighbor in assignment:
+                continue
+            i, j = self.crossword.overlaps[var, neighbor]
+            domain_neighbor = self.domains[neighbor]
+            for word_var in domain_var:
+                for word_neighbor in domain_neighbor:
+                    if word_var[i] == word_neighbor[j]:
+                        dictionary_for_ordering[word_var] += 1
+
+
+        ordering = [w for w, z in sorted(dictionary_for_ordering.items(), key = lambda item: item[1], reverse = True)]
+            
+        return ordering
+
+    def select_unassigned_variable(self, assignment):
+        """
+        Return an unassigned variable not already part of `assignment`.
+        Choose the variable with the minimum number of remaining values
+        in its domain. If there is a tie, choose the variable with the highest
+        degree. If there is a tie, any of the tied variables are acceptable
+        return values.
+        """
+         # Get set of unassigned variables
+        unassigned = set(self.domains.keys()) - set(assignment.keys())
+
+        # Create list of variables, sorted by MRV and highest degree
+        result = [var for var in unassigned]
+        result.sort(key = lambda x: (len(self.domains[x]), -len(self.crossword.neighbors(x))))
+
+        return result[0]
+
+
+    def backtrack(self, assignment):
+        """
+        Using Backtracking Search, take as input a partial assignment for the
+        crossword and return a complete assignment if possible to do so.
+
+        `assignment` is a mapping from variables (keys) to words (values).
+
+        If no assignment is possible, return None.
+        """
+
+        # If all variables are assigned, return assignment:
+        if self.assignment_complete(assignment):
+            return assignment
+
+        # Otherwise select an unassigned variable:
+        var = self.select_unassigned_variable(assignment)
+        for val in self.order_domain_values(var, assignment):
+            assignment[var] = val
+            if self.consistent(assignment):
+                result = self.backtrack(assignment)
+                if result:
+                    return result
+            del assignment[var]
+        return None
+
+def main():
+
+    # Check usage
+    if len(sys.argv) not in [3, 4]:
+        sys.exit("Usage: python generate.py structure words [output]")
+
+    # Parse command-line arguments
+    structure = sys.argv[1]
+    words = sys.argv[2]
+    output = sys.argv[3] if len(sys.argv) == 4 else None
+
+    # Generate crossword
+    crossword = Crossword(structure, words)
+    creator = CrosswordCreator(crossword)
+    assignment = creator.solve()
+
+    # Print result
+    if assignment is None:
+        print("No solution.")
+    else:
+        creator.print(assignment)
+        if output:
+            creator.save(assignment, output)
+
+
+if __name__ == "__main__":
+    main()
---
+++ shopping/shopping.py
@@ -0,0 +1,165 @@
+import csv
+import sys
+
+from sklearn.model_selection import train_test_split
+from sklearn.neighbors import KNeighborsClassifier
+
+TEST_SIZE = 0.4
+
+
+def main():
+
+    # Check command-line arguments
+    if len(sys.argv) != 2:
+        sys.exit("Usage: python shopping.py data")
+
+    # Load data from spreadsheet and split into train and test sets
+    evidence, labels = load_data(sys.argv[1])
+    X_train, X_test, y_train, y_test = train_test_split(
+        evidence, labels, test_size=TEST_SIZE
+    )
+
+    # Train model and make predictions
+    model = train_model(X_train, y_train)
+    predictions = model.predict(X_test)
+    sensitivity, specificity = evaluate(y_test, predictions)
+
+    # Print results
+    print(f"Correct: {(y_test == predictions).sum()}")
+    print(f"Incorrect: {(y_test != predictions).sum()}")
+    print(f"True Positive Rate: {100 * sensitivity:.2f}%")
+    print(f"True Negative Rate: {100 * specificity:.2f}%")
+
+
+def load_data(filename):
+    """
+    Load shopping data from a CSV file `filename` and convert into a list of
+    evidence lists and a list of labels. Return a tuple (evidence, labels).
+
+    evidence should be a list of lists, where each list contains the
+    following values, in order:
+        - Administrative, an integer
+        - Administrative_Duration, a floating point number
+        - Informational, an integer
+        - Informational_Duration, a floating point number
+        - ProductRelated, an integer
+        - ProductRelated_Duration, a floating point number
+        - BounceRates, a floating point number
+        - ExitRates, a floating point number
+        - PageValues, a floating point number
+        - SpecialDay, a floating point number
+        - Month, an index from 0 (January) to 11 (December)
+        - OperatingSystems, an integer
+        - Browser, an integer
+        - Region, an integer
+        - TrafficType, an integer
+        - VisitorType, an integer 0 (not returning) or 1 (returning)
+        - Weekend, an integer 0 (if false) or 1 (if true)
+
+    labels should be the corresponding list of labels, where each label
+    is 1 if Revenue is true, and 0 otherwise.
+    """
+    evidence = []
+    lable = []
+    with open(filename) as f:
+        reader = csv.reader(f)
+        next(reader)
+        for row in reader:
+            row_evidence = []
+            row_evidence.append(int(row[0]))
+            row_evidence.append(float(row[1]))
+            row_evidence.append(int(row[2]))
+            row_evidence.append(float(row[3]))
+            row_evidence.append(int(row[4]))
+            row_evidence.append(float(row[5]))
+            row_evidence.append(float(row[6]))
+            row_evidence.append(float(row[7]))
+            row_evidence.append(float(row[8]))
+            row_evidence.append(float(row[9]))
+            months = {
+                        "Jan": 0, 
+                        "Feb": 1, 
+                        "Mar": 2, 
+                        "Apr": 3, 
+                        "May": 4, 
+                        "June": 5,
+                        "Jul": 6, 
+                        "Aug": 7, 
+                        "Sep": 8, 
+                        "Oct": 9, 
+                        "Nov": 10, 
+                        "Dec": 11
+                    }
+            row_evidence.append(months[row[10]])
+            row_evidence.append(int(row[11]))
+            row_evidence.append(int(row[12]))
+            row_evidence.append(int(row[13]))
+            row_evidence.append(int(row[14]))
+            if row[15] == "Returning_Visitor":
+                row_evidence.append(1)
+            else:
+                row_evidence.append(0)
+
+            if row[16] == "TRUE":
+                row_evidence.append(1)
+            else:
+                row_evidence.append(0)
+            
+            evidence.append(row_evidence)
+
+            if row[17] == "TRUE":
+                lable.append(1)
+            else:
+                lable.append(0)
+
+    return (evidence, lable)
+
+
+def train_model(evidence, labels):
+    """
+    Given a list of evidence lists and a list of labels, return a
+    fitted k-nearest neighbor model (k=1) trained on the data.
+    """
+    model = KNeighborsClassifier(n_neighbors = 1)
+    model.fit(evidence, labels)
+    return model
+
+
+def evaluate(labels, predictions):
+    """
+    Given a list of actual labels and a list of predicted labels,
+    return a tuple (sensitivity, specificity).
+
+    Assume each label is either a 1 (positive) or 0 (negative).
+
+    `sensitivity` should be a floating-point value from 0 to 1
+    representing the "true positive rate": the proportion of
+    actual positive labels that were accurately identified.
+
+    `specificity` should be a floating-point value from 0 to 1
+    representing the "true negative rate": the proportion of
+    actual negative labels that were accurately identified.
+    """
+    correctly_labled_positive = 0
+    actual_positive = 0
+    correctly_labled_negative = 0
+    actual_negative = 0
+
+    for label, prediction in zip(labels, predictions):
+        if label == 1:
+            actual_positive += 1
+            if prediction == 1:
+                correctly_labled_positive += 1
+        else:
+            actual_negative += 1
+            if prediction == 0:
+                correctly_labled_negative += 1
+
+    sensitivity = float(correctly_labled_positive)/float(actual_positive)
+    specificity = float(correctly_labled_positive)/float(actual_negative)
+
+    return (sensitivity, specificity)
+
+
+if __name__ == "__main__":
+    main()
